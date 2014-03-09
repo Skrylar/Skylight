fa5a6f355cd7a4fa1d1ad085b0bbc96cd74f2a08
Joshua Cearley <joshua.cearley@gmail.com>
2014-03-09 01:22:55 -0600

    Add failing unit tests for FNV hashes.

--- skylight/fnv.nim
+++ skylight/fnv.nim
@@ -150,6 +150,65 @@ DefHash(uint64)
 # }}}
 
 # Unit testing {{{1
-# TODO
+
+when isMainModule:
+  import unittest
+
+  test "FNV-1 (32-bit)":
+    check Fnv1Hash32(""      ) == 0x811C9DC5'u32
+    check Fnv1Hash32("a"     ) == 0x050C5D7E'u32
+    check Fnv1Hash32("b"     ) == 0x050C5D7D'u32
+    check Fnv1Hash32("c"     ) == 0x050C5D7C'u32
+    check Fnv1Hash32("d"     ) == 0x050C5D7B'u32
+    check Fnv1Hash32("e"     ) == 0x050C5D7A'u32
+    check Fnv1Hash32("f"     ) == 0x050C5D79'u32
+    check Fnv1Hash32("fo"    ) == 0x6B772514'u32
+    check Fnv1Hash32("foo"   ) == 0x408F5E13'u32
+    check Fnv1Hash32("foob"  ) == 0xB4B1178B'u32
+    check Fnv1Hash32("fooba" ) == 0xFDC80FB0'u32
+    check Fnv1Hash32("foobar") == 0x31F0B262'u32
+
+  test "FNV-1a (32-bit)":
+    check Fnv1aHash32(""      ) == 0x811C9DC5'u32
+    check Fnv1aHash32("a"     ) == 0xE40C292C'u32
+    check Fnv1aHash32("b"     ) == 0xE70C2DE5'u32
+    check Fnv1aHash32("c"     ) == 0xE60C2C52'u32
+    check Fnv1aHash32("d"     ) == 0xE10C2473'u32
+    check Fnv1aHash32("e"     ) == 0xE00C22E0'u32
+    check Fnv1aHash32("f"     ) == 0xE30C2799'u32
+    check Fnv1aHash32("fo"    ) == 0x6222E842'u32
+    check Fnv1aHash32("foo"   ) == 0xA9F37ED7'u32
+    check Fnv1aHash32("foob"  ) == 0x3F5076EF'u32
+    check Fnv1aHash32("fooba" ) == 0x39AAA18A'u32
+    check Fnv1aHash32("foobar") == 0xBF9CF968'u32
+
+  test "FNV-1 (64-bit)":
+    check Fnv1Hash64(""      ) == 0xCBF29CE484222325'u64
+    check Fnv1Hash64("a"     ) == 0xAF63BD4C8601B7BE'u64
+    check Fnv1Hash64("b"     ) == 0xAF63BD4C8601B7BD'u64
+    check Fnv1Hash64("c"     ) == 0xAF63BD4C8601B7BC'u64
+    check Fnv1Hash64("d"     ) == 0xAF63BD4C8601B7BB'u64
+    check Fnv1Hash64("e"     ) == 0xAF63BD4C8601B7BA'u64
+    check Fnv1Hash64("f"     ) == 0xAF63BD4C8601B7B9'u64
+    check Fnv1Hash64("fo"    ) == 0x08326207B4EB2F34'u64
+    check Fnv1Hash64("foo"   ) == 0xD8CBC7186BA13533'u64
+    check Fnv1Hash64("foob"  ) == 0x0378817EE2ED65CB'u64
+    check Fnv1Hash64("fooba" ) == 0xD329D59B9963F790'u64
+    check Fnv1Hash64("foobar") == 0x340D8765A4DDA9C2'u64
+
+  test "FNV-1a (64-bit)":
+    check Fnv1aHash64(""      ) == 0xCBF29CE484222325'u64
+    check Fnv1aHash64("a"     ) == 0xAF63DC4C8601EC8C'u64
+    check Fnv1aHash64("b"     ) == 0xAF63DF4C8601F1A5'u64
+    check Fnv1aHash64("c"     ) == 0xAF63DE4C8601EFF2'u64
+    check Fnv1aHash64("d"     ) == 0xAF63D94C8601E773'u64
+    check Fnv1aHash64("e"     ) == 0xAF63D84C8601E5C0'u64
+    check Fnv1aHash64("f"     ) == 0xAF63DB4C8601EAD9'u64
+    check Fnv1aHash64("fo"    ) == 0x08985907B541D342'u64
+    check Fnv1aHash64("foo"   ) == 0xDCB27518FED9D577'u64
+    check Fnv1aHash64("foob"  ) == 0xDD120E790C2512AF'u64
+    check Fnv1aHash64("fooba" ) == 0xCAC165AFA2FEF40A'u64
+    check Fnv1aHash64("foobar") == 0x85944171F73967E8'u64
+
 # }}}
 
