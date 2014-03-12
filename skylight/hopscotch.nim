b34e6b43867c35c179b50ba11702a5911b714f8c
Joshua Cearley <joshua.cearley@gmail.com>
2014-03-12 00:57:13 -0500

    Fixed everything except grow and linear probing.

--- skylight/hopscotch.nim
+++ skylight/hopscotch.nim
@@ -20,6 +20,15 @@ type
 
 # Internal code {{{1
 
+proc Reset [K,V](self: var HopscotchNode[K,V]) =
+  zeroMem(addr(self), sizeof(HopscotchNode[K,V]))
+
+iterator HopOffsets(mask: uint32): int =
+  for x in 30..0:
+    let y = uint32(1 shl x)
+    if (mask and y) > 0'u32:
+      yield (30-x)
+
 proc DoInsert [K,V](self: var HopscotchTable[K,V]; key: K; value: V): bool =
   # TODO: We should probably make the algorithm adjustable.
   let hash   = Siphash24(key)
@@ -33,35 +42,50 @@ proc DoInsert [K,V](self: var HopscotchTable[K,V]; key: K; value: V): bool =
   else:
     # We're adding a child to this element.
     for offset in 0..31:
-      # check if a good place was found
-      if self.Database[bucket+offset].Mask == 0:
-        self.Database[bucket+offset].Mask     = (1 shl 31)
-        self.Database[bucket+offset].LocalKey = key
-        self.Database[bucket+offset].Value    = value
-        # now mark the parent with this knowledge
-        self.Database[bucket].Mask =
-          self.Database[bucket].Mask or (uint32(1) shl uint32(offset))
-        # we're good
-        inc(self.Elements)
-        return true
+      if bucket+offset < self.Database.len:
+        # TODO support bouncing people out of the neighborhood
+        # check if a good place was found
+        if self.Database[bucket+offset].Mask == 0:
+          self.Database[bucket+offset].Mask     = (1 shl 31)
+          self.Database[bucket+offset].LocalKey = key
+          self.Database[bucket+offset].Value    = value
+          # now mark the parent with this knowledge
+          self.Database[bucket].Mask =
+            self.Database[bucket].Mask or (uint32(1) shl uint32(offset))
+          # we're good
+          inc(self.Elements)
+          return true
+      else:
+        return false
   return false
 
 # }}}
 
 # Construction {{{1
 
-proc InitHopscotchTable*(self: var HopscotchTable) =
+proc InitHopscotchTable* [K,V](self: var HopscotchTable[K,V]) =
   self.MaximumSize = 0
   self.Elements    = 0
   self.Database    = @[]
   self.Database.setLen(1024)
+  zeroMem(addr(self.Database[0]),
+    self.Database.len * sizeof(HopscotchNode[K,V]) )
 
 # }}}
 
 # Element access {{{1
 
-proc TryGet* [K,V](self: var HopscotchTable[K,V]; key: K; outValue: V): bool =
-  return false
+proc TryGet* [K,V](self: var HopscotchTable[K,V]; key: K; outValue: var V): bool =
+  let hash   = Siphash24(key)
+  let bucket = int(hash mod uint64(self.Database.len))
+  if self.Database[bucket].Mask == 0:
+    return false
+  else:
+    if self.Database[bucket].LocalKey == key:
+      outValue = self.Database[bucket].Value
+      return true
+    else:
+      quit "TODO linear probe"
 
 proc `[]`* [K,V](self: var HopscotchTable[K,V]; key: K): V =
   if not self.TryGet(key, result):
@@ -76,7 +100,14 @@ proc `[]=`* [K,V](self: var HopscotchTable[K,V]; key: K; value: V) =
         "Cannot fit new element, even after rehashing.")
 
 proc Del* [K,V](self: var HopscotchTable[K,V]; key: K) =
-  discard
+  let hash   = Siphash24(key)
+  let bucket = int(hash mod uint64(self.Database.len))
+  if self.Database[bucket].Mask == 0: return
+  elif self.Database[bucket].Mask == (1 shl 31):
+    self.Database[bucket].Reset
+    dec(self.Elements)
+  else:
+    quit "TODO linear deletion probe"
 
 template Delete* [K,V](self: var HopscotchTable[K,V]; key: K) =
   Del(self, key)
@@ -86,7 +117,7 @@ template Delete* [K,V](self: var HopscotchTable[K,V]; key: K) =
 # Querying {{{1
 
 proc Len* [K,V](self: var HopscotchTable[K,V]): int =
-  return 0 # TODO
+  return self.Elements
 
 # }}}
 
@@ -123,7 +154,8 @@ when isMainModule:
     check table.len == 2
 
     checkpoint "retrieval"
-    result = table.TryGet(57, output) == true
+    result = table.TryGet(57, output)
+    check result == true
     check output == "i am soup"
     check table[57] == "i am soup"
 
